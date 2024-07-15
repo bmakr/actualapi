05672f35440a3de5951252ff44e43ee3e4f25c45
B Mack <lumeo@me.com>
2024-07-15 15:15:56 -0400

    add cors

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "https://www.actuali.net" }, // replace this your actual origin
           { key: "Access-Control-Allow-Methods", value: "POST" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
